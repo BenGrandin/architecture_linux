0ccb31147e438d2e4864505ec5f7287eadc45ac1
Ben <bengrandin@hotmail.com>
2020-01-14 14:59:53 +0100

    Initial commit

---
+++ Script/script.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+    printf("coucou\n");
+    printf("nb d'arg= %d \n", argc);
+    for(int i = 0; i < argc; i++){
+        printf("%s\n", argv[i]);
+    }
+    char c;
+    while(read(0, &c,1) == 1){
+        write(2,&c,1);
+    }
+    return 0;
+}
---
+++ Script/script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a comment!
+echo Hello World
+
+toto=5
+echo $  toto
+
+toto=$((toto+1))
+echo $toto
