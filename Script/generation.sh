1bc8d4d9d9b772bf6a59aca4f6c6f800ca285936
Ben <bengrandin@hotmail.com>
2020-01-15 18:41:22 +0100

    Script/generation.js: add 2 args

--- Script/generation.sh
+++ Script/generation.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+if [ "$1" -eq "0" ]; then
+  echo "No argument 1 supplied"
+  errorFile="errors.txt"
+else
+  errorFile=$1
+fi
 
- ./genTick 500 | ./genSensorData
+if [ "$1" -eq "0" ]; then
+  echo "No argument 2 supplied"
+  logsFile="logs.txt"
+else
+  logsFile=$1
+fi
+
+echo "$logsFile"
+echo "$errorFile"
+
+./genTick 1000 | ./genSensorData 2>>errorFile >>logsFile
