3e285b1e055f185bbb304a8302569c2352e15e77
Squaaw <damien.jisseau@edu.itescia.fr>
2020-01-16 16:34:08 +0100

    Story 7 : generation.sh

--- Script/generation.sh
+++ Script/generation.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-#Forcing the use of 3 parameters
-if [ "$#" -eq "0" ]; then
-  echo "No argument supplied, need 3"
-elif [ "$#" -eq "1" ]; then
-   echo "1 arguments supplied, need 3"
-elif [ "$#" -eq "2" ]; then
-  echo "2 arguments supplied, need 3"
+if [ $USER == "jisseaudamien" ] || [ $USER == "grandinbenjamin" ] || [ $USER == "duhalgouetmelec" ]; then
+  #Forcing the use of 3 parameters
+  if [ "$#" -eq "0" ]; then
+    echo "No argument supplied, need 3"
+  elif [ "$#" -eq "1" ]; then
+    echo "1 arguments supplied, need 3"
+  elif [ "$#" -eq "2" ]; then
+    echo "2 arguments supplied, need 3"
+  else
+    echo "alright"
+    echo "$USER"
+
+  #Dynamic creation of Directory and log and error files
+    cd /
+    sudo mkdir -p "$1"
+    sudo touch ./"$1"/"$2".txt
+    sudo touch ./"$1"/"$3".txt
+
+    chmod +x ./Users/$USER/Desktop/architecture_linux/Script/genTick
+    chmod +x ./Users/$USER/Desktop/architecture_linux/Script/genSensorData
+
+    sudo gcc ./Users/$USER/Desktop/architecture_linux/Script/genTick.c -o genTick
+    sudo gcc ./Users/$USER/Desktop/architecture_linux/Script/genSensorData.c -o genSensorData
+
+  #writing in log and error files, >> write at the end of the files to keep previous logs
+    ./genTick 500 | ./genSensorData >> ./"$1"/"$2".txt 2>> ./"$1"/"$3".txt
+    # ./genTick 500 | ./genSensorData >> ./Users/$USER/Desktop/architecture_linux/Script/"$1"/"$2".txt 2>> ./Users/$USER/Desktop/architecture_linux/Script/"$1"/"$3".txt
+    # ./genTick 100 | ./genSensorData 2>> ./"$1"/"$2".txt | stdbuf -oL cut -d';' -f2 > ./"$1"/"$3".txt
+    # ./genTick 100 | ./genSensorData 2>> $3/$1 | stdbuf -oL cut -d'#' -f2 >> $3/$2
+  fi
+
 else
-  echo "alright"
-
-#Dynamic creation of Directory and log and error files
-  mkdir -p "$1"
-  touch ./"$1"/"$2".txt
-  touch ./"$1"/"$3".txt
-
-  chmod +x genTick
-  chmod +x genSensorData
-
-  gcc genTick.c -o genTick
-  gcc genSensorData.c -o genSensorData
-
-#writing in log and error files, >> write at the end of the files to keep previous logs
-  ./genTick 500 | ./genSensorData >> ./"$1"/"$2".txt 2>> ./"$1"/"$3".txt
+  echo "ACCESS DENIED"
 fi
